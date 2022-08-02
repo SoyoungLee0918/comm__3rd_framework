772d7ef9305d3e7e4f2751014cd8610fd202e70c
sihwa1019 <epretty@naver.com>
2022-08-02 11:23:07 +0900

    articleController를 Container를 통해서 얻는다.

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -1,10 +1,18 @@
 package com.ll.exam;
 
+import com.ll.exam.article.controller.ArticleController;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest {
+    @Test
+    public void ioc__articleController() {
+        ArticleController articleController = Container.getArticleController();
+
+        assertThat(articleController).isNotNull();
+    }
+
     @Test
     public void junit_assertThat() {
         int rs = 10 + 20;
